a3190f6690b3898ecafc9c76aceeb5ffa8a0ff61
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2022-12-12 17:13:46 -0700

    adding files to repo

---
+++ javascript.js
@@ -0,0 +1,49 @@
+$( function() {
+    $("#accordion").accordion();
+  } );
+$( function() {
+    $(".accordion").accordion({
+          collapsible: true, active: false
+      });
+  });
+
+$( function() {
+    $("#datepicker").datepicker();
+    });
+$(document).ready(function(){
+    $("#start").click(function(){
+        $("#number10").fadeIn(2000);
+    });
+    $("#btn1").click(function(){
+        $("#number9").fadeIn(2000);
+    });
+    $("#btn2").click(function(){
+        $("#number8").fadeIn(2000);
+    });
+    $("#btn3").click(function(){
+        $("#number7").fadeIn(2000);
+    });
+    $("#btn4").click(function(){
+        $("#number6").fadeIn(2000);
+    });
+    $("#btn5").click(function(){
+        $("#number5").fadeIn(2000);
+    });
+    $("#btn6").click(function(){
+        $("#number4").fadeIn(2000);
+    });
+    $("#btn7").click(function(){
+        $("#number3").fadeIn(2000);
+    });
+    $("#btn8").click(function(){
+        $("#number2").fadeIn(2000);
+    });
+    $("#btn9").click(function(){
+        $("#number1").fadeIn(2000);
+    });
+    $("#btn10").click(function(){
+        $("#end").fadeIn(2000);
+    });
+}); 
+
+
